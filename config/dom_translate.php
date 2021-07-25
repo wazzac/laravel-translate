180c9fa98adc74eebc5419ca67b0616ca4b022b4
Warren Coetzee <warren.coetzee@gmail.com>
2021-07-25 23:06:45 +0800

    Updated the Config file.

--- config/dom_translate.php
+++ config/dom_translate.php
@@ -2,21 +2,21 @@
 // return config
 return [
     'api' => [
-        'provider' => "google",
+        'provider' => env('DOM_TRANSLATE_PROVIDER', 'google'),
         'google' => [
             'endpoint' => "https://www.googleapis.com/language/translate/v2",
             'action' => "POST",
-            'key' => "AIzaSyBuZ_9lrMTGydfE7j-4EnMRU6PTd0q2p4k", // you will have to generate your own
+            'key' => env('DOM_TRANSLATE_GOOGLE_KEY'), // https://console.cloud.google.com/apis/credentials
         ],
         // add more translate providers here...
     ],
     'hash' => [
-        'salt' => "zBQ2DxKhNa",
-        'algo' => "sha1", // https://www.php.net/manual/en/function.hash-algos.php
+        'salt' => env('DOM_TRANSLATE_HASH_SALT', 'zBQ2DxKhNa'),
+        'algo' => env('DOM_TRANSLATE_HASH_ALGO', 'sha1'), // https://www.php.net/manual/en/function.hash-algos.php
     ],
     // Only ISO-639-1 format: @link: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
     'language' => [
-        'src' => 'en',
-        'dest' => 'af',
+        'src' => env('DOM_TRANSLATE_LANG_SRC', 'en'), // default source language
+        'dest' => env('DOM_TRANSLATE_LANG_DEST', 'af'), // default destication language
     ]
 ];
