e995ebe2377ca7f8f274a0acb23a4c75806240cb
Warren Coetzee <warren.coetzee@gmail.com>
2021-08-03 22:27:41 +0800

    Created 2 new Factories that will primarily be used in test cases.

---
+++ database/factories/PhraseFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Wazza\DomTranslate\Phrase;
+use Wazza\DomTranslate\Controllers\TranslateController;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Phrase::class, function (Faker $faker) {
+    $phrase = $faker->sentence(40);
+    $hash = \Wazza\DomTranslate\Helpers\phraseHelper::hash($phrase);
+    return [
+        'language_id' => config('dom_translate.language.src'),
+        'hash' => $phrase,
+        'value' => $hash,
+        'created_at' => Carbon\Carbon::now(),
+        'updated_at' => Carbon\Carbon::now(),
+    ];
+});
---
+++ database/factories/TranslationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Wazza\DomTranslate\Translation;
+use Wazza\DomTranslate\Language;
+use Wazza\DomTranslate\Phrase;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Translation::class, function (Faker $faker) {
+    return [
+        'language_id' => config('dom_translate.language.dest'),
+        'phrase_id' => Phrase::inRandomOrder()->first(),
+        'value' => $faker->sentence(40),
+        'created_at' => Carbon\Carbon::now(),
+        'updated_at' => Carbon\Carbon::now(),
+    ];
+});
