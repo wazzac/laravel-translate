b388b086c8a28dae92d40928d24fdf5bd7cc4d5a
Warren Coetzee <warren.coetzee@gmail.com>
2024-07-20 18:14:17 +0800

    develop/v2.2.1: Renamed the PhraseHelper class.

--- database/factories/PhraseFactory.php
+++ database/factories/PhraseFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Wazza\DomTranslate\Helpers\phraseHelper;
+use Wazza\DomTranslate\Helpers\PhraseHelper;
 use Wazza\DomTranslate\Phrase;
 use Wazza\DomTranslate\Language;
 
@@ -15,7 +15,7 @@ class PhraseFactory extends Factory
     public function definition()
     {
         $phrase = $this->faker->sentence(40);
-        $hash = phraseHelper::hash($phrase);
+        $hash = PhraseHelper::hash($phrase);
 
         return [
             'language_id' => Language::where('code', config('dom_translate.language.src'))->first()->id,
